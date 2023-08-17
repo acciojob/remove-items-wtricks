dabf470b7af0a6bae33c19042391a799d0901a69
Anuj Kumar <101552403+wtricks@users.noreply.github.com>
2023-08-17 10:03:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const button = document.querySelector("form input[type='button']");
+const options = document.querySelector("#colorSelect");
+
+button.onclick = () => {
+	// document.body.append(document.createTextNode(options.value));
+	const value = options.value;
+	document.querySelectorAll("option").forEach(e => {
+		if (e.textContent.trim() == value) {
+			options.removeChild(e);
+		}
+	})
+}
